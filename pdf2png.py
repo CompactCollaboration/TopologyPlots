d8165678c51d4f09a97d5a702fb9bd546015529a
Deyan Mihaylov <d.p.mihaylov@gmail.com>
2026-05-24 14:57:58 -0400

    Add E16h

--- pdf2png.py
+++ pdf2png.py
@@ -3,7 +3,7 @@ from pdf2image import convert_from_path
 for topology in [
 	"E1", "E2", "E3", "E4", "E5", "E6",
 	"E7", "E8","E9", "E10", "E11", "E12",
-	"E13", "E14", "E15", "E16i", "E17", "E18",
+	"E13", "E14", "E15", "E16h", "E16i", "E17", "E18",
 ]:
 	try:
 		image = convert_from_path(
